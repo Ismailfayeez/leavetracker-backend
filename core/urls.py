3207e8cc7537f94faf2a1f2f1840d8deded8ff93
mohammed ismail fayeez <mohammedismailfayeez@gmail.com>
2023-03-18 14:09:12 +0530

    removed comments

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.views.generic import TemplateView
 urlpatterns = [
-    # not-verified
     path('', TemplateView.as_view(template_name='core/index.html'))
 ]
