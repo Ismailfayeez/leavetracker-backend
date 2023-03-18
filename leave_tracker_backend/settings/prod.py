c896a15733f47187f18fbd64b6b23912437f76b1
mohammed ismail fayeez <mohammedismailfayeez@gmail.com>
2023-03-18 19:12:36 +0530

    Added requirements.txt and database config in prod settings

--- leave_tracker_backend/settings/prod.py
+++ leave_tracker_backend/settings/prod.py
@@ -1,8 +1,13 @@
 from .common import *
 import os
-
+import dj_database_url
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
 ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
